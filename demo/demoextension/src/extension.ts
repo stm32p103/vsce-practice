fe895b5e06842f39c9612d2ebc6442a9f5b0539e
stm32p103 <stm32p103@gmail.com>
2020-05-12 00:16:36 +0900

    デモのメニュー追加

--- demo/demoextension/src/extension.ts
+++ demo/demoextension/src/extension.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
+import { DemoProvider, Demo } from './demo-navigation';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -13,15 +13,24 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('demoextension.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('demo.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from DemoExtension!');
 	});
+	
+	vscode.commands.registerCommand('demo.open', (demo: Demo) => {
+		vscode.window.showInformationMessage(`${demo.title}`);
+	});
 
+	const demoProvder = new DemoProvider();
+
+	vscode.window.registerTreeDataProvider('demo.list', new DemoProvider());
 	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	console.log( 'deactivated' );
+}
